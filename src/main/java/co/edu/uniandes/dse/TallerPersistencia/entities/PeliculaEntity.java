53137e19f331fd8d6b92764fbbbf4ba1c91c420d
Jorge Sebastian Otalora Bernal <js.otalorab12@uniandes.edu.co>
2026-02-11 08:39:49 -0500

    Punto 3 resuelto

---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/PeliculaEntity.java
@@ -0,0 +1,12 @@
+package co.edu.uniandes.dse.TallerPersistencia.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class PeliculaEntity extends BaseEntity {
+    private String titulo;
+    private Integer anioLanzamiento;  
+    
+}
